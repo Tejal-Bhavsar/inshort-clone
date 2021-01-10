719ac1e2737968759cac99b1d724b2168f8b38f9
tejal-bhavsar-au6 <tejalbhavsar02@gmail.com>
2021-01-11 01:13:29 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import { spacing } from '@material-ui/system';
+import Box from '@material-ui/core/Box';
+
 
 export default function Footer() {
     return (
@@ -7,6 +12,11 @@ export default function Footer() {
                  inshorts clone  made by -{" "}
                  <a href="https://www.linkedin.com/in/tejal-bhavsar-ba0130173/">Tejal Bhavsar</a>
              </span>
+             <hr />
+             <Box px={4} className="footercontainer">
+                 <a href="https://github.com/Tejal-Bhavsar" ><GitHubIcon fontSize="large"  /></a>
+                 <a href="https://www.linkedin.com/in/tejal-bhavsar-ba0130173/"><LinkedInIcon fontSize="large"  /></a>
+             </Box>
         </div>
     )
 }
